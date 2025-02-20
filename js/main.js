7eb64355a9b6c030b2619cff7f3266261d63373b
Esteban Padilla <ep@estebanpadilla.com>
2025-02-20 17:36:32 -0600

    Add files

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/**
+* @name main.js
+* @file Add a small description for this file.
+* @author Esteban Padilla, ep@estebanpadilla.com
+* @version 1.0.0
+*/
+
+"use strict";
+
+window.addEventListener('load', init, false);
+
+function init() {
+    console.log('App running!');
+    //1. Declare variables
+    //2. Initialize variables
+    //3. Events
+    //4. Program Logic
+}
